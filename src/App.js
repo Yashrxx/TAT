4f8277e7743324ecbedc2a5e365c9225e4f059af
Yashrxx <yashkumar.s2023@vitstudent.ac.in>
2025-06-10 11:00:12 +0530

    Update package.json and App.js for deployment

--- src/App.js
+++ src/App.js
@@ -8,21 +8,22 @@ import FullDress from './dataValues/FullDress';
 import About from './components/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter ,Route , Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
+  
   return (
-  <BrowserRouter basename="/TAT">
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/measurements' element={<Measurements/>}/>
-      <Route path='/top' element={<Top/>}/>
-      <Route path='/fulldress' element={<FullDress/>}/>
-      <Route path='/bottom' element={<Bottom/>}/>
-      <Route path='/about' element={<About/>}/>
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/measurements' element={<Measurements />} />
+        <Route path='/top' element={<Top />} />
+        <Route path='/fulldress' element={<FullDress />} />
+        <Route path='/bottom' element={<Bottom />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
