ab9ff30a4e687c8097b16af32497f3da0efb912b
Yash Jain <yashsushillunkad313@gmail.com>
2025-06-15 15:34:52 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 app.use(cors({
-  // origin: 'https://yashrxx.github.io',
-  origin : 'http://localhost:3001',
+  origin: 'https://yashrxx.github.io',
+  // origin : 'http://localhost:3001',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
@@ -17,7 +17,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.options('*', cors());
+app.options('*any', cors());
 
 app.get("/", (req, res) => {
     res.status(200).json({ success: true, message: "formFiller Backend is Live!" });
