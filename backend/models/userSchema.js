fbb5dc78b6c466e1013662a90e8da6d9006d88ae
yashrxx <yashsushillunkad313@gmail.com>
2025-06-17 22:26:56 +0530

    debugged createuser route

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -20,10 +20,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  cpassword: {
-    type: String,
-    required: true
-  },
   date: {
     type: Date,
     default: Date.now
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,39 @@
 const express=require('express')
 const router=express.Router()
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { DrsM, Top, Bottom } = require('../models/drsM.js');
 const User = require('../models/userSchema.js');
 
-router.post('/user', async (req, res) => {
-  const { name, number, address } = req.body;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post('/createuser', async (req, res) => {
+  const { name, email, phone, password } = req.body;
+
   try {
-    const user = await User.create({ name, number, address });
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json({ error: 'Error creating user' });
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ success: false, error: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(password, salt);
+
+    user = await User.create({
+      name,
+      email,
+      phone,
+      password: secPass
+    });
+
+    const data = { id: user._id };
+    const authToken = jwt.sign(data, JWT_SECRET);
+
+    res.json({ success: true, authToken });
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
   }
 });
 
